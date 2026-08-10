7caf055e66da23ae654da3f6e58fd6f2f07f561e
Bizuayehu18 <bizuayehuasefa25@gmail.com>
2026-08-10 19:07:19 +0300

    keep staging preflight login temporary

--- infra/sql/staging-beta-runtime-disable.sql
+++ infra/sql/staging-beta-runtime-disable.sql
@@ -11,3 +11,24 @@ alter role payreplayy_beta_admission_runtime with
   connection limit 1
   password null
   valid until 'infinity';
+
+do $payreplayy$
+begin
+  if not exists (
+    select 1
+    from pg_catalog.pg_authid as role
+    where role.rolname = 'payreplayy_beta_admission_runtime'
+      and not role.rolcanlogin
+      and not role.rolinherit
+      and not role.rolsuper
+      and not role.rolcreatedb
+      and not role.rolcreaterole
+      and not role.rolreplication
+      and not role.rolbypassrls
+      and role.rolconnlimit = 1
+      and role.rolpassword is null
+  ) then
+    raise exception 'The staging beta-admission runtime login was not disabled safely.';
+  end if;
+end
+$payreplayy$;

b7da525aa40e46507d1cad07087bda03d8aba45e
Bizuayehu18 <bizuayehuasefa25@gmail.com>
2026-09-18 15:31:02 +0300

    Match provider guard cardinality

--- supabase/migrations/20260918040000_allow_post_emergency_live_telebirr_historical_completion.sql
+++ supabase/migrations/20260918040000_allow_post_emergency_live_telebirr_historical_completion.sql
@@ -559,7 +559,7 @@ select pg_temp.apply_exact_historical_function_patch(
       )
     )$replacement$
   ],
-  array[1, 1, 3, 3]
+  array[1, 1, 3, 2]
 );
 
 select pg_temp.apply_exact_historical_function_patch(
